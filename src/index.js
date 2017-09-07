a56621308b2d55cdbe988b95f5728929dfbf656b
Roberto Brito <brituor@gmail.com>
2017-09-06 20:40:50 -0700

    Serving an HTML Template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Hapi from 'hapi';
+import nunjucks from 'nunjucks';
+
+// configure nunjucks to read from the dist directory
+nunjucks.configure('./dist');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -12,7 +16,13 @@ server.route({
     method: 'GET',
     path:'/hello',
     handler: function (request, reply) {
-       reply('hello world');
+        // read template and compile using context object
+        nunjucks.render('index.html', {
+            fname: 'Rick', lname: 'Sanchez'
+        }, function(err, html) {
+            // reply with HTML response
+            reply(html);
+        });
     }
 });
 
