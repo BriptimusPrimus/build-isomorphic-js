aea2f570c5a1e3a0b0bf80f42076af2f55a0848c
Roberto Brito <brituor@gmail.com>
2017-09-06 22:31:45 -0700

    Extending the Controller Improving the Response Flow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Hapi from 'hapi';
 import Application from './lib';
 import Controller from './lib/controller';
+import HelloController from './HelloController';
+import nunjucks from 'nunjucks';
+
+// configure nunjucks to read from the dist directory
+nunjucks.configure('./dist');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
@@ -10,9 +15,18 @@ server.connection({
 });
 
 const application = new Application({
-    '/': Controller
+    '/': Controller,
+    '/hello/{name*}': HelloController
 }, {
-    server: server
+    server: server,
+    document: function (application, controller, request, reply, body, callback) {
+        nunjucks.render('./index.html', { body: body }, (err, html) => {
+          if (err) {
+            return callback(err, null);
+          }
+          callback(null, html);
+        });
+    }
 });
 
 // Start the server
