b770e1f8cc0fa3bd8187f34ada70e82e1afe9ec6
Roberto Brito <brituor@gmail.com>
2017-09-06 20:53:38 -0700

    Working with Path and Query Parameters

--- src/index.js
+++ src/index.js
@@ -11,15 +11,34 @@ server.connection({
     port: 8000
 });
 
+function getName(request) {
+  // default values
+  let name = {
+    fname: 'Rick',
+    lname: 'Sanchez'
+  };
+  // split path params
+  let nameParts = request.params.name ? request.params.name.split('/') : [];
+
+  // order of precedence
+  // 1. path param
+  // 2. query param
+  // 3. default value
+  name.fname = (nameParts[0] || request.query.fname) ||
+    name.fname;
+  name.lname = (nameParts[1] || request.query.lname) ||
+    name.lname;
+
+  return name;
+}
+
 // Add the route
 server.route({
     method: 'GET',
-    path:'/hello',
+    path:'/hello/{name*}',
     handler: function (request, reply) {
         // read template and compile using context object
-        nunjucks.render('index.html', {
-            fname: 'Rick', lname: 'Sanchez'
-        }, function(err, html) {
+        nunjucks.render('index.html', getName(request), function(err, html) {
             // reply with HTML response
             reply(html);
         });
