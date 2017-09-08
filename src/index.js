8c5680848e533f03d01cf738d0a919dca1403a68
Roberto Brito <brituor@gmail.com>
2017-09-07 21:30:23 -0700

    Adding Our Client Implementation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Application from './lib';
 import Controller from './lib/controller';
 import HelloController from './HelloController';
 import nunjucks from 'nunjucks';
+import Inert from 'inert';
 
 // configure nunjucks to read from the dist directory
 nunjucks.configure('./dist');
@@ -13,20 +14,33 @@ server.connection({
     host: 'localhost',
     port: 8000
 });
+server.register(Inert, () => {});
 
+const APP_FILE_PATH = '/application.js';
 const application = new Application({
     '/': Controller,
     '/hello/{name*}': HelloController
 }, {
     server: server,
     document: function (application, controller, request, reply, body, callback) {
-        nunjucks.render('./index.html', { body: body }, (err, html) => {
+        nunjucks.render('./index.html', {
+            body: body,
+            application: APP_FILE_PATH
+        }, (err, html) => {
           if (err) {
             return callback(err, null);
           }
           callback(null, html);
         });
     }
+});
+
+server.route({
+  method: 'GET',
+  path: APP_FILE_PATH,
+  handler: (request, reply) => {
+    reply.file('dist/build/application.js');
+  }
 });
 
 // Start the server
